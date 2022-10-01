0a5d97864bd9fcbf0e30cd18b3b8eb981e8ab179
SU-T <guitaristpeet@gmail.com>
2022-09-30 17:43:20 -0700

    clean up

--- index.js
+++ index.js
@@ -1,33 +1,37 @@
-for (i = 0; i < 10; i++)
-{
-    // create div with a class of 'row'
-    let div = document.createElement('div');
-    div.classList.add('row');
-    
-    // create multiple span of each letter in the word 'stretch'
-    const word = 'spread';
-    let wordArray = [...word];
-    wordArray.forEach((letter) => {
-        let htmlEl = '';
-        htmlEl += `<span class="letter">${letter}</span>`;
-        div.innerHTML += htmlEl;
-    });
-    
-    // Add created words to the article container
-    const container = document.querySelector('.container');
-    container.appendChild(div);
-}
+document.addEventListener('DOMContentLoaded', () => {
+    for (i = 0; i < 10; i++)
+    {
+        // create div with a class of 'row'
+        let div = document.createElement('div');
+        div.classList.add('row');
+        
+        // create multiple span of each letter in the word 'stretch'
+        const word = 'spread';
+        let wordArray = [...word];
+        wordArray.forEach((letter) => {
+            let htmlEl = '';
+            htmlEl += `<span class="letter">${letter}</span>`;
+            div.innerHTML += htmlEl;
+        });
+        
+        // Add created words to the article container
+        const container = document.querySelector('.container');
+        container.appendChild(div);
+    }
+    // animate text
+    gsap.to('.letter', {
+        fontStretch: '125%',
+        fontWeight: '600',
+        letterSpacing: '-3.5vw',
+        stagger: {
+            each: 0.03,
+            from: 'center',
+            grid: 'auto',
+        },
+        repeat: -1,
+        yoyo: true,
+        ease: 'power2.inOut',
+    }) 
+});
 
-gsap.to('.letter', {
-    fontStretch: '125%',
-    fontWeight: '600',
-    letterSpacing: '-3.5vw',
-    stagger: {
-        each: 0.03,
-        from: 'center',
-        grid: 'auto',
-    },
-    repeat: -1,
-    yoyo: true,
-    ease: 'power2.inOut',
-}) 
+
