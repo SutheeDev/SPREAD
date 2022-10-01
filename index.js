8591e1ff3b4b4cfd6b9e4ec5dd2c01fd0a1cc824
SU-T <guitaristpeet@gmail.com>
2022-09-30 17:34:33 -0700

    create multiple words

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
-let div = document.createElement('div');
-div.classList.add('row');
-
-const word = 'spread';
-let wordArray = [...word];
-wordArray.forEach((letter) => {
-    let htmlEl = '';
-    htmlEl += `<span class="letter">${letter}</span>`;
-    div.innerHTML += htmlEl;
-});
-
-const container = document.querySelector('.container');
-container.appendChild(div);
+for (i = 0; i < 10; i++)
+{
+    // create div with a class of 'row'
+    let div = document.createElement('div');
+    div.classList.add('row');
+    
+    // create multiple span of each letter in the word 'stretch'
+    const word = 'spread';
+    let wordArray = [...word];
+    wordArray.forEach((letter) => {
+        let htmlEl = '';
+        htmlEl += `<span class="letter">${letter}</span>`;
+        div.innerHTML += htmlEl;
+    });
+    
+    // Add created words to the article container
+    const container = document.querySelector('.container');
+    container.appendChild(div);
+}
